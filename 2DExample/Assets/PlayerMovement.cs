9c24fba85b0c58500975fddc74066bca77ea001d
Patlak <onurpa@at-work.local>
2021-04-26 22:04:11 +0200

    Test Project created

---
+++ 2DExample/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+    private Rigidbody2D _rigidbody;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce),ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ 2DExample/Assets/TestChar/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator anim;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            anim.SetTrigger("attack");
+        }
+
+    }
+}
+
